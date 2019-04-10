c4e553bd15fea6d441e3e3e600fbf11128322a5b
RCY <1223356413@qq.com>
2019-04-10 16:59:34 +0800

    沒有用到springboot 直接运行测试类即可带有中文注释

---
+++ src/main/java/com/rcy/springbootgenerator/SpringbootGeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.rcy.springbootgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootGeneratorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootGeneratorApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/rcy/springbootgenerator/config/EmptyCommentGenerator.java
@@ -0,0 +1,133 @@
+package com.rcy.springbootgenerator.config;
+
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @Auther: RCY
+ * @Date: 2019/4/10 15:22
+ * @Description:
+ * @Email: 18600820674@163.com
+ */
+
+public class EmptyCommentGenerator implements CommentGenerator {
+    // 属性，即配置在 commentGenerator 标签之内的 Property 标签
+    private Properties properties;
+    // 是否不生成日期
+    private boolean suppressDate;
+    // 是否不生成注释
+    private boolean suppressAllComments;
+    // 是否添加数据库内的注释
+    private boolean addRemarkComments;
+    // 日期格式化
+    private SimpleDateFormat dateFormat;
+
+    public EmptyCommentGenerator() {
+        super();
+        properties = new Properties();
+        suppressDate = false;
+        suppressAllComments = false;
+        addRemarkComments = false;
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
+
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+}
